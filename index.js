87cd5cd6771b97fe6ca2f8daebf06c6c36f6a31c
Martins Michael <clefmaj@gmail.com>
2020-12-13 04:58:32 +0100

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+// const form = document.getElementById("form");
+// const password = document.getElementById("password");
+
+// form.addEventListener("submit", (e) => {
+//     let messages = []
+
+//     e.preventDefault()
+// })
